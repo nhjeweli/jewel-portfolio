b468a27b42d24893df9a631a58cf84a3ca3fc16a
nhjewel <nhjewel75@yahoo.com>
2024-07-30 23:16:08 +0600

    some problem solved

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -144,7 +144,7 @@ const Work: React.FC = () => {
               })}
 
               {/** slider navigation */}
-              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all" />
+              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all" iconsStyles="font-[20px]" />
             </Swiper>
           </div>
         </div>
